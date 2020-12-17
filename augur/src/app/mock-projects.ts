f44fb83bea9ba4cf2122ebf2933d3c5649da87c7
Luke Austin <Luke@lukes-macbook-pro.home>
2020-12-17 14:08:01 +0100

    fixing scroll for mobile... hopefully

--- augur/src/app/mock-projects.ts
+++ augur/src/app/mock-projects.ts
@@ -5,13 +5,13 @@ export const PROJECTS : Project[] = [
         id: 1, year: 2018, description: "A traditional tile based game", 
         title: "Honshu", code: "https://gitlab.com/jonath20e/Honshu", demo: "", language: "C", tool: "libsdl",
         image: "honshu.png",
-        details: "This project was created with 3 other students. The idea of the game is to place tiles and gain points. The project also contains a solver. The included screenshot is the console version of the game because libsdl isn't working on my machine."
+        details: "This project was created with 3 other students. The idea of the game is to place tiles and gain points. The project also contains a solver. The included screenshot is the console version of the game."
     },
     {
         id: 2, year: 2019, description: "A 3d action roguelike", 
         title: "Biome", code: "https://gitlab.com/Atchoom/biome", demo: "https://www.youtube.com/watch?v=RRjrrGLzHUc", language: "C#", tool: "Unity",
         image: "biome.png",
-        details: "Created with four other students, Biome is a video game where the goal is to get from the center of a procedurally generated world to the outer rim, all while battling a series of increasingly challenging enemies. The game is still in development."
+        details: "Created with four other students, Biome is a video game in which the goal is to get from the center of a procedurally generated world to the outer rim, all while battling a series of increasingly challenging enemies. The game is still in development."
     },
     {
         id: 3, year: 2018, description: "The classic arcade game", 
@@ -47,17 +47,17 @@ export const PROJECTS : Project[] = [
         id: 8, year: 2018, description: "A procedurally generated labyrinth", 
         title: "Labyrinth", code: "https://github.com/gitlukeaustin/caml", demo: "", language: "OCaml", tool: "",
         image: "",
-        details: "The labyrinth could be on a hexagon or square grid. It was created to learn about recursion and fonctional programming."
+        details: "The labyrinth could be on a hexagonal or square grid. It was created to learn about recursion and fonctional programming."
     },
     {
         id: 9, year: 2015, description: "A game about exploring a library", 
         title: "Library Game", code: "", demo: "http://game.lib.dept.ku.edu/?fbclid=IwAR0t8Y9_6XhOMAw8yyJsYrKs5s3ZSaW7MuPjhy4MjnsOytYSJdgk9SkziU4", language: "HTML", tool: "Twine",
         image: "library.png",
-        details: "Created with three other students, this was the first game I've ever participated in creating. It was made with Twine so hardly any coding was involved. It became used by the University of Kansas as an educational tool. You can play it by following the Demo link."
+        details: "Created with three other students, this was my first foray into video game development. It eventually was expanded on and used by the University of Kansas as an educational tool. You can play it by following the Demo link."
     },
     {
         id: 11, year: 2019, description: "A video game about avoiding raptors",
-        title: "Raptor Game", code:"", demo:"", image:"",language:"C++",tool:"OpenGL",
+        title: "Frogland Saga", code:"https://gitlab.com/gitlukeaustin/opengl-jin", demo:"", image:"",language:"C++",tool:"OpenGL",
         details:"Created in a 3d rendering class, the goal of this simple game is to collect apples and avoid raptors."
     }
     /*{
